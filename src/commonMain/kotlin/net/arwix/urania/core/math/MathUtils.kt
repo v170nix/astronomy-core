6824a8af25b6e60bad0f9b9c0762d55c79dcd029
Vitaliy Sheyanov <vit.onix@gmail.com>
2021-12-30 00:28:33 +0300

    add mod3600

--- src/commonMain/kotlin/net/arwix/urania/core/math/MathUtils.kt
+++ src/commonMain/kotlin/net/arwix/urania/core/math/MathUtils.kt
@@ -1,6 +1,10 @@
 package net.arwix.urania.core.math
 
+import kotlin.math.floor
+
 inline fun DoubleArray.polynomialSum(x: Double): Double {
     var t = 1.0
     return fold(0.0) { acc, d -> (acc + d * t).let { t *= x; it } }
 }
+
+inline fun Double.mod3600() = this - 1296000.0 * floor(this / 1296000.0)
