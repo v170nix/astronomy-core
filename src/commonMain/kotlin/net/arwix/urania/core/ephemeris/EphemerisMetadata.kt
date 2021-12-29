73ede01dd1ce3c538a9c93270010b9c607dcfb19
Vitaliy Sheyanov <vit.onix@gmail.com>
2021-12-29 20:47:13 +0300

    change enum to sealed

--- src/commonMain/kotlin/net/arwix/urania/core/ephemeris/EphemerisMetadata.kt
+++ src/commonMain/kotlin/net/arwix/urania/core/ephemeris/EphemerisMetadata.kt
@@ -1,8 +1,28 @@
 package net.arwix.urania.core.ephemeris
 
-enum class Orbit { Geocentric, Heliocentric }
-enum class Plane { Ecliptic, Equatorial, Azimuthal }
-enum class Epoch { J2000, Apparent }
+import net.arwix.urania.core.math.vector.Vector
+import kotlin.js.JsExport
 
+@JsExport
+sealed class Orbit {
+    object Geocentric: Orbit()
+    object Heliocentric: Orbit()
+}
 
+@JsExport
+sealed class Plane {
+    object Ecliptic: Plane()
+    object Equatorial: Plane()
+    object Azimuthal: Plane()
+}
+
+@JsExport
+sealed class Epoch {
+    object J2000: Epoch()
+    object Apparent: Epoch()
+}
+
+@JsExport
 data class EphemerisMetadata(val orbit: Orbit, val plane: Plane, val epoch: Epoch)
+
+data class EphemerisVector(val value: Vector, val metadata: EphemerisMetadata)
