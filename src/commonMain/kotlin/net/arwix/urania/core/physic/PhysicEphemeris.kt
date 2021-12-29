9b0429c080e70a327e8b6c27c567d08335cb3b5d
Vitaliy Sheyanov <vit.onix@gmail.com>
2021-12-29 20:48:39 +0300

    don't use

---
+++ src/commonMain/kotlin/net/arwix/urania/core/physic/PhysicEphemeris.kt
@@ -0,0 +1,34 @@
+package net.arwix.urania.core.physic
+
+import net.arwix.urania.core.annotation.ExperimentalUrania
+import net.arwix.urania.core.calendar.JT
+import net.arwix.urania.core.kepler.KeplerElementsObject
+import net.arwix.urania.core.math.vector.Matrix
+import net.arwix.urania.core.math.vector.RectangularVector
+
+
+@ExperimentalUrania
+class PhysicEphemeris {
+
+//    private var jT: JT
+//    private var P: Matrix
+//    private var Ecl2EquMatrix
+//
+//    private var r_Earth: RectangularVector
+//    private var body: KeplerElementsObject
+//
+//    public var apparentDiameter: Double
+//    public var visualMagnitude: Double
+//    public var i: Double?
+//
+//    var PositionAngleSun: Double?
+//
+//    var PositionAngleA: Double
+//
+//    var B: Double? = null
+//
+//    var L_I: Double? = null
+//    var L_II: Double? = null
+//    var L_III: Double? = null
+
+}
