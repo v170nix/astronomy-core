d2b968ef49538c177905ef1b6dd7c23b10f3a98a
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-01-03 12:46:37 +0300

    add dun radius

--- src/commonMain/kotlin/net/arwix/urania/core/ephemeris/Constants.kt
+++ src/commonMain/kotlin/net/arwix/urania/core/ephemeris/Constants.kt
@@ -1,3 +1,7 @@
+@file:Suppress("unused")
+
 package net.arwix.urania.core.ephemeris
 
 const val AU = 149597870.7    // 1ае
+const val SUN_RADIUS_IN_ARCS = 959.644 // in arc-seconds at 1ae
+const val SUN_RADIUS_IN_METERS = 695508000.0
