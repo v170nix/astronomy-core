1e1539aff70a3abd60b0d23ac56d6e1402933626
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-01-04 17:38:16 +0300

    add date time dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
             }
         }
         val commonTest by getting {
