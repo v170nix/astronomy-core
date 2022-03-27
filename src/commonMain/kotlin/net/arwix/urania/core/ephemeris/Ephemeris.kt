98bfa4cd064e0e8716d4f488a68cd267f188a8aa
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-03-27 23:52:49 +0300

    remove ephemeris cache

--- src/commonMain/kotlin/net/arwix/urania/core/ephemeris/Ephemeris.kt
+++ src/commonMain/kotlin/net/arwix/urania/core/ephemeris/Ephemeris.kt
@@ -2,10 +2,6 @@
 
 package net.arwix.urania.core.ephemeris
 
-import kotlinx.coroutines.*
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import net.arwix.urania.core.annotation.ExperimentalUrania
 import net.arwix.urania.core.calendar.JT
 import net.arwix.urania.core.calendar.jT
 import net.arwix.urania.core.math.JULIAN_DAYS_PER_CENTURY
@@ -22,37 +18,4 @@ interface Ephemeris {
         val bodyPlus = invoke(jT + (deltaTimeInDay / JULIAN_DAYS_PER_CENTURY).jT)
         return (bodyPlus - vectorAtJTTime) / deltaTimeInDay
     }
-}
-
-@ExperimentalUrania
-class WrapperCacheEphemeris(private val ephemeris: Ephemeris): Ephemeris {
-    override val metadata: Metadata = ephemeris.metadata
-    private val cache = mutableMapOf<JT, Vector>()
-    private val mutex = Mutex()
-    private val computationMap = mutableMapOf<JT, Deferred<Vector>>()
-
-    override suspend fun invoke(jT: JT): Vector = coroutineScope {
-        val cacheElement: Vector? = cache[jT]
-        if (cacheElement == null) {
-            val currentDeferred = mutex.withLock { computationMap[jT] }
-            if (currentDeferred != null) return@coroutineScope currentDeferred.await()
-
-            val deferred = async(Dispatchers.Default, start = CoroutineStart.LAZY) { ephemeris(jT) }
-            val newResult = mutex.withLock {
-                val currentDeferred2 = computationMap[jT]
-                if (currentDeferred2 == null) {
-                    computationMap[jT] = deferred
-                    deferred.start()
-                    deferred
-                } else currentDeferred2
-            }.await()
-            cache[jT] = newResult
-            @Suppress("DeferredResultUnused")
-            mutex.withLock {
-                computationMap.remove(jT)
-            }
-            newResult
-        } else cacheElement
-    }
-
 }
