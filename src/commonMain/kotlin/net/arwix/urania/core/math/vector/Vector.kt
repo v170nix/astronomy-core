4fbae24d20f278d850667ef71bfe52d26430b71c
Vitaliy Sheyanov <vit.onix@gmail.com>
2022-04-17 18:11:52 +0300

    add angular distance add position angle

--- src/commonMain/kotlin/net/arwix/urania/core/math/vector/Vector.kt
+++ src/commonMain/kotlin/net/arwix/urania/core/math/vector/Vector.kt
@@ -1,6 +1,16 @@
+@file:Suppress("unused")
+
 package net.arwix.urania.core.math.vector
 
+import net.arwix.urania.core.math.angle.Radian
+import net.arwix.urania.core.math.angle.cos
+import net.arwix.urania.core.math.angle.rad
+import net.arwix.urania.core.math.angle.sin
+import net.arwix.urania.core.rectangular
+import net.arwix.urania.core.spherical
 import kotlin.js.JsName
+import kotlin.math.acos
+import kotlin.math.atan2
 import kotlin.math.sqrt
 
 
@@ -42,4 +52,52 @@ interface Vector {
     operator fun component3(): Double
 
     fun equalsVector(other: Vector): Boolean
+
+    companion object {
+
+        /**
+         * Obtain angular distance between two spherical coordinates.
+         *
+         * @return The distance in radians, from 0 to PI.
+         */
+        inline fun getAngularDistance(vector1: SphericalVector, vector2: SphericalVector): Radian {
+            val v1 = SphericalVector(vector1.phi, vector1.theta).rectangular
+            val v2 = SphericalVector(vector2.phi, vector2.theta).rectangular
+
+            val dx = v1[0] - v2[0]
+            val dy = v1[1] - v2[1]
+            val dz = v1[2] - v2[2]
+
+            val r2 = dx * dx + dy * dy + dz * dz
+
+            return acos(1.0 - r2 * 0.5).rad
+        }
+
+        /**
+         * Obtain exact position angle between two spherical coordinates.
+         *
+         * @return The position angle in radians.
+         */
+        inline fun getPositionAngle(vector1: SphericalVector, vector2: SphericalVector): Radian {
+            val dl = vector2.theta - vector1.theta
+            val cbp: Double = cos(vector2.phi)
+            val y: Double = sin(dl) * cbp
+            val x: Double = sin(vector2.phi) * cos(vector1.phi) - cbp * sin(vector1.phi) * cos(dl)
+            return if (x != 0.0 || y != 0.0) -atan2(y, x).rad else Radian.Zero
+        }
+    }
 }
+
+/**
+ * Obtain angular distance between two spherical coordinates.
+ *
+ * @return The distance in radians, from 0 to PI.
+ */
+inline fun Vector.getAngularDistance(other: Vector): Radian = Vector.getAngularDistance(this.spherical, other.spherical)
+
+/**
+ * Obtain exact position angle between two spherical coordinates.
+ *
+ * @return The position angle in radians.
+ */
+inline fun Vector.getPositionAngle(other: Vector): Radian = Vector.getPositionAngle(this.spherical, other.spherical)
