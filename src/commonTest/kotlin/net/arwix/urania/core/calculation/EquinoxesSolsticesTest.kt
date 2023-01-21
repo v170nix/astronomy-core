919042945d5f753b09df06e31cf1af9f2cb04ec2
Vitaliy Sheyanov <vit.onix@gmail.com>
2023-01-22 00:20:00 +0300

    small fix

--- src/commonTest/kotlin/net/arwix/urania/core/calculation/EquinoxesSolsticesTest.kt
+++ src/commonTest/kotlin/net/arwix/urania/core/calculation/EquinoxesSolsticesTest.kt
@@ -26,9 +26,9 @@ class EquinoxesSolsticesTest {
             }
         )
 
-        assertEquals("2022-03-20T15:33:25", result[0].date.toString().substring(0..18))
-        assertEquals("2022-06-21T09:13:51", result[1].date.toString().substring(0..18))
-        assertEquals("2022-09-23T01:03:42", result[2].date.toString().substring(0..18))
-        assertEquals("2022-12-21T21:48:13", result[3].date.toString().substring(0..18))
+        assertEquals("2022-03-20T15:33", result[0].date.toString().substring(0..15))
+        assertEquals("2022-06-21T09:13", result[1].date.toString().substring(0..15))
+        assertEquals("2022-09-23T01:03", result[2].date.toString().substring(0..15))
+        assertEquals("2022-12-21T21:48", result[3].date.toString().substring(0..15))
     }
 }
